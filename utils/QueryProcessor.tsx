09a2c6147cc459479398773c27ffff079be7ff27
Hiba Hamad <hibahamad@dyn-172-20-97-206.qatar.cmu.local>
2024-10-24 16:23:46 +0300

    name

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -12,5 +12,9 @@ export default function QueryProcessor(query: string): string {
     //TODO update the corresponding test case in __tests__
     return ( "hhamad" );
   }
+
+  if (query.toLowerCase().includes("your name")) {
+    return "My name is QueryProcessor.";
+  }
   return "";
 }
